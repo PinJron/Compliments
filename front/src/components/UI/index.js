fd994999e99eea179f2e6e4d8433150339e0d4de
Dimitry <dimitry.zelencko@yandex.ru>
2023-08-18 01:45:57 +0300

    v1.1

--- front/src/components/UI/index.js
+++ front/src/components/UI/index.js
@@ -1,13 +1,13 @@
 import MyButton from "@/components/UI/MyButton.vue";
 import MyLikeButton from "@/components/UI/MyLikeButton.vue";
-import MydislikeButton from "@/components/UI/MydislikeButton.vue";
+import MyDislikeButton from "@/components/UI/MyDislikeButton.vue";
 import MyInput from "@/components/UI/MyInput.vue";
 import MyOutput from "@/components/UI/MyOutput.vue";
 
 export default [
   MyButton,
   MyLikeButton,
-  MydislikeButton,
+  MyDislikeButton,
   MyInput,
   MyOutput
 ]
